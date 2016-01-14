de9e17988117ff395ede1ce6b176b77e3c721917
Shiva Malhotra <Shiva.malhotra.email@gmail.com>
2016-01-13 16:59:04 -0800

    something

---
+++ src/robotname.java
@@ -0,0 +1,60 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class robotname {
+public static void main(String[] args) {
+	Robot bob = new Robot();
+	bob.setRandomPenColor();
+	bob.setSpeed(10);
+	bob.turn(270);
+	bob.penDown();
+	bob.move(50);
+	bob.turn(90);
+bob.move(200);
+	bob.turn(180);
+bob.move(200);
+	bob.turn(270);
+	bob.move(50);
+	bob.penUp();
+	bob.move(50);
+	bob.penDown();
+bob.turn(270);	
+	bob.move(200);
+	bob.turn(90);
+	bob.move(100);
+	bob.turn(90);
+bob.move(200);
+bob.turn(90);	
+	bob.move(100);
+	bob.turn(180);
+	bob.move(100);
+	bob.penUp();
+	bob.move(50);
+bob.penDown();
+	bob.move(100);
+	bob.turn(180);
+	bob.move(100);
+	bob.turn(90);
+	bob.move(200);
+	bob.turn(90);
+	bob.move(100);
+	bob.turn(180);
+bob.move(100);	
+	bob.turn(270);
+	bob.move(200);
+bob.turn(270);
+bob.move(100);
+	bob.penUp();
+	bob.move(50);
+	bob.penDown();
+	bob.turn(270);
+	bob.move(200);
+	bob.turn(180);
+	bob.move(100);
+	bob.turn(270);
+	bob.move(100);
+	bob.turn(90);
+	bob.move(100);
+	bob.turn(180);
+bob.move(200);	
+}
+}
